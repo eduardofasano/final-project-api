2d0777ea6739539044148443aa0fb3dc8c9af4cb
eduardofasano <eduardo.fasano.93@gmail.com>
2016-12-09 10:57:01 +0000

    updated db:seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,110 @@ stilton = Product.create!({
   image: "http://i.dailymail.co.uk/i/pix/2013/03/30/article-0-18605F9F000005DC-421_634x433.jpg"
 })
 
+gorgonzola = Product.create!({
+  name: "Gorgonzola",
+  description: "The most delicious Gorgonzola cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "https://www.winefetch.com/images/cheese/big_topicimage236.jpg"
+})
+
+taleggio = Product.create!({
+  name: "Taleggio",
+  description: "The most delicious Taleggio cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://www.jjzf.dk/wp-content/uploads/2015/02/taleggio.jpg"
+})
+
+burrata = Product.create!({
+  name: "Burrata",
+  description: "The most delicious Burrata cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://www.italianfoodexport.es/wp-content/uploads/2015/06/burrata.jpg"
+})
+
+crescenza = Product.create!({
+  name: "Crescenza",
+  description: "The most delicious Crescenza cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://www.italianfoodexport.es/wp-content/uploads/2015/06/burrata.jpg"
+})
+
+mozzarella = Product.create!({
+  name: "Mozzarella",
+  description: "The most delicious Mozzarella cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://www.cucinare.meglio.it/img/notizie/mozzarella-di-bufala.jpg"
+})
+
+pecorino = Product.create!({
+  name: "Pecorino",
+  description: "The most delicious Pecorino cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://gastronicks.co.uk/wp-content/uploads/2013/03/Normanno.jpg"
+})
+
+parmiggiano = Product.create!({
+  name: "Pecorino",
+  description: "The most delicious Pecorino cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://www.italianherkut.fi/751/parmiggiano-reggiano-24-mesi-1-kg.jpg"
+})
+
+caciocavallo = Product.create!({
+  name: "Caciocavallo",
+  description: "The most delicious Caciocavallo cheese of all the land",
+  seller_id: emily.id,
+  quantity: 30,
+  unit:"kg",
+  max_price:12.0,
+  min_price:10.0,
+  # final_price:
+  duration: 120,
+  image: "http://shop.superlatticini.it/WebRoot/StoreIT5/Shops/16631/5176/930B/AD05/97A3/0056/3E95/9311/C8DB/Caciocavallo_affumicato_S.jpg"
+})
+
 Order.create!({
   product_id: stilton.id,
   buyer_id: mike.id,
